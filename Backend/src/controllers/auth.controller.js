d19ddb5ccaa420ecc5c4c79d23111180ac47965c
Shivam Prajapati <shivamkumar152240@gmail.com>
2026-06-08 20:34:07 +0530

    fix production cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -36,7 +36,12 @@ async function userControllerRegister(req, res) {
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-    res.cookie("token", token)
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
 
     res.status(201).json({
         message: "user register successfully",
@@ -75,7 +80,12 @@ async function userControllerLogin(req, res) {
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-    res.cookie("token", token)
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
     res.status(200).json({
         message: "User LoggedIn successfully",
         user: {
@@ -97,7 +107,11 @@ async function userControllerLogout(req, res) {
          await tokenBlackListModel.create({ token })
         
     }
-    res.clearCookie("token")
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
     res.status(200).json({
         message: "user logout successfully"
     })
